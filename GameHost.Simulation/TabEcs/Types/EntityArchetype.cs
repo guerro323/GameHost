d9cd34a797eb0868a69922bd23596a18cc0f08fc
Guerro323 <kaltobattle@gmail.com>
2021-08-06 12:16:01 +0200

    added EntityArchetype equality methods

--- GameHost.Simulation/TabEcs/Types/EntityArchetype.cs
+++ GameHost.Simulation/TabEcs/Types/EntityArchetype.cs
@@ -1,12 +1,29 @@
-﻿namespace GameHost.Simulation.TabEcs
+﻿using System;
+
+namespace GameHost.Simulation.TabEcs
 {
-	public readonly struct EntityArchetype
+	public readonly struct EntityArchetype : IEquatable<EntityArchetype>
 	{
 		public readonly uint Id;
 
 		public EntityArchetype(uint id)
 		{
 			Id = id;
+		}
+
+		public bool Equals(EntityArchetype other)
+		{
+			return Id == other.Id;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is EntityArchetype other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return (int)Id;
 		}
 	}
 }
