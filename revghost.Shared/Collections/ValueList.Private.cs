afe9d3c8915d4924ed8fcd902d62de4b5756f7a6
Guerro323 <kaltobattle@gmail.com>
2021-11-17 23:53:15 +0100

    Add ValueList

---
+++ revghost.Shared/Collections/ValueList.Private.cs
@@ -0,0 +1,40 @@
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace revghost.Shared.Collections;
+
+public partial struct ValueList<T>
+{
+    [MemberNotNull(nameof(_array))]
+    private void Allocate(int size)
+    {
+        if (_array != null)
+        {
+            ArrayPool<T>.Shared.Return(_array, _containsReference);
+
+            var old = _array;
+            _array = ArrayPool<T>.Shared.Rent(size);
+
+            Array.Copy(old, _array, old.Length);
+        }
+
+        _array = ArrayPool<T>.Shared.Rent(size);
+    }
+
+    [MemberNotNull(nameof(_array))]
+    private void Resize(int newSize)
+    {
+        var length = _array?.Length ?? 0;
+        if (newSize <= length)
+        {
+            _array = Array.Empty<T>();
+            return;
+        }
+
+        var target = length == 0 ? 16 : length * 2;
+        if (target < newSize)
+            target = newSize;
+
+        Allocate(target);
+    }
+}
---
+++ revghost.Shared/Collections/ValueList.cs
@@ -0,0 +1,147 @@
+using System.Buffers;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace revghost.Shared.Collections;
+
+public partial struct ValueList<T> : IList<T>, IDisposable
+{
+    private static bool _containsReference = RuntimeHelpers.IsReferenceOrContainsReferences<T>();
+
+    private T[]? _array = Array.Empty<T>();
+    private int _count;
+
+    public Span<T> Span => _array.AsSpan(0, _count);
+
+    public ValueList(int capacity)
+    {
+        _array = ArrayPool<T>.Shared.Rent(capacity);
+        _count = 0;
+    }
+
+    public bool Remove(T item)
+    {
+        var index = IndexOf(item);
+        if (index < 0)
+            return false;
+
+        RemoveAt(index);
+        return true;
+    }
+
+    public int Count
+    {
+        get => _count;
+        set => _count = value;
+    }
+
+    bool ICollection<T>.IsReadOnly => false;
+
+    public void Add(T item)
+    {
+        var count = _count;
+
+        _count += 1;
+        Resize(_count);
+
+        _array[count] = item;
+    }
+
+    public void Clear()
+    {
+        _count = 0;
+        if (_containsReference && _array != null)
+        {
+            Span.Clear();
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        return IndexOf(item) >= 0;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Span<T>.Enumerator GetEnumerator() => Span.GetEnumerator();
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int IndexOf(T item)
+    {
+        if (_count == 0)
+            return -1;
+
+        return Array.IndexOf(_array!, item, 0, _count);
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index >= _count)
+        {
+            _count = index + 1;
+            Resize(_count);
+
+            _array[index] = item;
+        }
+        else
+        {
+            Array.Copy(_array!, index, _array!, index + 1, _count - index);
+            _array![index] = item;
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index > _count)
+            throw new ArgumentOutOfRangeException();
+
+        _count -= 1;
+        if (index < _count)
+        {
+            Array.Copy(_array!, index + 1, _array!, index, _count - index);
+        }
+
+        if (_containsReference)
+        {
+            _array![index] = default!;
+        }
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (_count == 0)
+                throw new IndexOutOfRangeException();
+
+            return _array![index];
+        }
+        set
+        {
+            if (index >= _count)
+                throw new IndexOutOfRangeException();
+
+            _array![index] = value;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_array != null)
+        {
+            ArrayPool<T>.Shared.Return(_array, _containsReference);
+        }
+    }
+}
